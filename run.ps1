f0190f68bb681b92950d63b607642ff30e4f632d
bucrogers <buc@acm.org>
2022-09-10 15:09:06 -0400

    add missing files from last commit

--- run.ps1
+++ run.ps1
@@ -4,8 +4,8 @@ if ( $args[0] -eq "stop" ) {
     docker-compose down
 }
 else {
-    # Start server, frontend locally
-    docker-compose up -d
+    # Start server, frontend locally, build from source with layer caching
+    docker-compose up -d --build
 
     # Launch browser on frontend
     Start-Process $URL
