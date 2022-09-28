5c56eb397f84bb81dde7ef8113504a97d14910b6
bucrogers <buc@acm.org>
2022-09-28 11:56:22 -0400

    fix newlines

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# WSL note: For launch-browser behavior via xdg-open, install https://github.com/cpbotha/xdg-open-wsl
+# Windows WSL note: For launch-browser behavior via xdg-open, install https://github.com/cpbotha/xdg-open-wsl
 
 URL=http://localhost:3000
 
