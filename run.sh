586eac9363d16a1e300e0a9a3e2bd035ebacccce
bucrogers <buc@acm.org>
2022-09-28 11:49:00 -0400

    add bash strict err handling

--- run.sh
+++ run.sh
@@ -4,20 +4,24 @@
 
 URL=http://localhost:3000
 
-if [ "$1" = "stop" ]
+set -euo pipefail
+
+if [ "${1:-}" = "stop" ]
 then
   docker-compose down
 else
-  if [ "$1" != "--build" ]
+  if [ "${1:-}" != "--build" ]
   then
     # Pull image if not already present
+    set +e # allow offline failure of pull
     docker-compose pull
+    set -e
   fi
 
   # Start server, frontend locally, build from source with layer caching
   export DOCKER_TZ=$(ls -la /etc/localtime | sed -r  "s/.*\/([^\/]+)\/([^\/]+)$/\1\/\2/")
   echo "DOCKER_TZ: ${DOCKER_TZ}"
-  docker-compose up -d $1
+  docker-compose up -d ${1:-}
 
   # Launch browser on frontend
   if which open > /dev/null
